4882ea0f1b3c497c0269831e4c023ab207caa1a4
Abdelrahman Bahaa <67162997+sweetpotatoX@users.noreply.github.com>
2024-04-18 09:25:44 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
+"""Module for task 0"""
 
-import requests
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    if response.status_code == 200:
-        try:
-            data = response.json()
-            subscribers = data['data']['subscribers']
-            return subscribers
-        except KeyError:
-            # If the 'subscribers' key is not found in the JSON response
-            print("Error: 'subscribers' key not found in JSON response.")
-            return None
-    elif response.status_code == 404:
-        # If the subreddit does not exist
-        print("Error: Subreddit '{}' does not exist.".format(subreddit))
-        return None
-    else:
-        # If the request fails for any other reason
-        print("Error: Request failed with status code {}".format(response.status_code))
-        return None
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        return 0
+
+    return sub_info.json().get("data").get("subscribers")
