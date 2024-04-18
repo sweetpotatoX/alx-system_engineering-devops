cfed5f99652d0a69a8ba8c62b049a417e6b1c941
Abdelrahman Bahaa <67162997+sweetpotatoX@users.noreply.github.com>
2024-04-18 09:21:59 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,15 +5,26 @@ Script that queries subscribers on a given Reddit subreddit.
 
 import requests
 
-
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
+    
     if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+        try:
+            data = response.json()
+            subscribers = data['data']['subscribers']
+            return subscribers
+        except KeyError:
+            # If the 'subscribers' key is not found in the JSON response
+            print("Error: 'subscribers' key not found in JSON response.")
+            return None
+    elif response.status_code == 404:
+        # If the subreddit does not exist
+        print("Error: Subreddit '{}' does not exist.".format(subreddit))
+        return None
     else:
-        return 0
+        # If the request fails for any other reason
+        print("Error: Request failed with status code {}".format(response.status_code))
+        return None
